108f108520fc09159ae35b47b0c472571c186b87
Myles Megyesi <mylesmegyesi@me.com>
2011-11-28 23:58:17 -0600

    First Commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject httpserver "0.1"
+  :description "Http Server"
+  :dev-dependencies [[speclj "1.2.0"]]
+	:test-path "spec/"
+  :java-source-path "src/"
+  )
---
+++ src/httpserver/HttpServer.java
@@ -0,0 +1,61 @@
+package httpserver;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class HttpServer {
+
+    public HttpServer() {
+
+    }
+
+    public HttpServer(String directory) {
+        this.setDirectory(directory);
+    }
+
+    public HttpServer(int port) {
+        this.setPort(port);
+    }
+
+    public HttpServer(String directory, int port) {
+        this.setDirectory(directory);
+        this.setPort(port);
+    }
+
+    public String getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(String directory) {
+        this.directory = directory;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        synchronized (socketLock) {
+            serverSocket = new ServerSocket(this.getPort());
+            serverSocket.setReuseAddress(true);
+        }
+    }
+
+    public void stop() throws IOException {
+        synchronized (socketLock) {
+            if (serverSocket != null) {
+                serverSocket.close();
+                serverSocket = null;
+            }
+        }
+    }
+
+    private String directory = ".";
+    private int port = 8080;
+    private ServerSocket serverSocket;
+    private Object socketLock = new Object();
+}
